c5f3cbfe9c37d3a977e1a5c2e753fc9f16bea9c7
Diego Dias <53064194+Diego-sdd@users.noreply.github.com>
2022-06-18 22:00:22 -0300

    deleted key map
    deleted key map

--- Donation/src/components/InputAddressSearch/index.tsx
+++ Donation/src/components/InputAddressSearch/index.tsx
@@ -31,7 +31,7 @@ const InputAddressSearch: React.FC<Type> = ({required, onPress, label}) => {
         getDefaultValue={() => ''}
         enablePoweredByContainer={false}
         query={{
-          key: 'AIzaSyDZsIm0N2GgKqnsPWbbrgxmMGGt0MKpJ7Y',
+          key: '',
           language: 'pt-BR', // language of the results
         }}
         nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
